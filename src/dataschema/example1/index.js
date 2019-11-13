7c3418f01524b437bf05dc09348d8bdd0b563ece
burakbasbug <burak.basbug@scout24.com>
2019-11-13 20:43:49 +0100

    add example1

---
+++ src/dataschema/example1/index.js
@@ -0,0 +1,47 @@
+const MQTT = require('async-mqtt');
+const log = require('yalm');
+const faker = require('faker');
+const config = require('../../../config');
+
+log.setLevel(config.logLevel);
+const client = MQTT.connect('mqtt://localhost:8883');
+
+// When passing async functions as event listeners, make sure to have a try catch block
+const publish = async data => {
+  try {
+    await client.publish('example1', JSON.stringify(data));
+    // This line doesn't run until the server responds to the publish
+    // await client.end();
+    // This line doesn't run until the client has disconnected without error
+  } catch (e) {
+    log.err(e.stack);
+    process.exit();
+  }
+};
+
+const onConnect = async () => {
+  log.info('Connected');
+};
+client.on('connect', onConnect);
+
+const exampleTypes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
+const generateData = () => {
+  const random6 = Math.floor(Math.random() * 6) + 1;
+  faker.seed(random6);
+  return {
+    id: `Sensor_${random6}`, // string
+    value: Math.random() * 100, // number
+    location: {
+      lat: (faker.address.latitude() % 10) + 45 + random6, // number
+      lon: (faker.address.longitude() % 12) + 5 + random6, // number
+    },
+    type: exampleTypes[Math.floor(Math.random() * 10)], // keyword
+    timestamp: new Date().getTime(), // ISODate
+  };
+};
+
+setInterval(async () => {
+  await publish(generateData());
+}, 300);
+// island: 65.795278, -22.884514
+// m.east 38.071867, 49.933342
